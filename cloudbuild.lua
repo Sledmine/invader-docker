84a92c0ac4835ddd28fe24b00d3e287ac4f2c0e3
Sledmine <40512223+Sledmine@users.noreply.github.com>
2022-02-02 16:53:16 -0700

    update docker image name

--- cloudbuild.lua
+++ cloudbuild.lua
@@ -6,7 +6,7 @@ local split = glue.string.split
 -- local inspect = require "lua.inspect"
 
 local dockerRun =
-    [[sudo docker run -v "/home/ubuntu/ShadowmodsCloud/nextcloud/data/%s/files/%s/tags":/home/invader/tags -v "%s":/home/invader/maps -v "%s":/home/invader/resources invaderdocker ]]
+    [[sudo docker run -v "/home/ubuntu/ShadowmodsCloud/nextcloud/data/%s/files/%s/tags":/home/invader/tags -v "%s":/home/invader/maps -v "%s":/home/invader/resources invader-docker ]]
 
 -- invader-build commands for CI/CD server
 -- user, cloudpath, outputpath, resourcespath, tagsize, scenariopath
